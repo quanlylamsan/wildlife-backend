6a936b69a1332ee7ee774900a69b82350d71b811
PHUC NGUYEN <phuc88.klvn@gmail.com>
2025-08-10 18:19:54 +0700

    Final nx

--- models/WoodActivity.js
+++ models/WoodActivity.js
@@ -15,6 +15,7 @@ const WoodActivitySchema = new mongoose.Schema({
   date: { // Ngày nhập/xuất
     type: Date,
     required: true,
+    trim: true,
   },
   type: { // Loại giao dịch: 'import' (nhập), 'export' (xuất)
     type: String,
@@ -40,10 +41,7 @@ const WoodActivitySchema = new mongoose.Schema({
   destination: { // Nơi đến (chỉ cho xuất)
     type: String,
     trim: true,
-    required: function () {
-      return this.type === 'export';
-    }
-  },
+  }, // <-- Dấu đóng ngoặc tròn ')' bị thiếu ở đây
   verifiedBy: { // Người xác nhận
     type: String,
     trim: true,
